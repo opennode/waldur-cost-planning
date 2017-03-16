3e4382dc5c645057c3b34c2edf0cc4640e9b9a2b
Pavel Marchuk <marchukpavelp@gmail.com>
2017-03-16 15:07:18 +0200

    Migrate filters to Django 1.9

--- src/nodeconductor_cost_planning/filters.py
+++ src/nodeconductor_cost_planning/filters.py
@@ -1,22 +1,16 @@
 import django_filters
 
-from nodeconductor.core.filters import UUIDFilter
 from . import models
 
 
 class DeploymentPlanFilter(django_filters.FilterSet):
-    customer = UUIDFilter(
+    customer = django_filters.UUIDFilter(
         name='customer__uuid',
         distinct=True,
+    )
+    o = django_filters.OrderingFilter(
+        fields=('name', 'created')
     )
 
     class Meta(object):
         model = models.DeploymentPlan
-        fields = ['customer']
-
-        order_by = [
-            'name',
-            '-name',
-            'created',
-            '-created'
-        ]
