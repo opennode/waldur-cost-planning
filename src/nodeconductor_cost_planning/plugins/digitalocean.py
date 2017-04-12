59cb62ceb7a1dafcc94afa21be063f6a4162cfe4
Pavel Marchuk <marchukpavelp@gmail.com>
2017-04-12 11:36:15 +0300

    Use cost tracking prices for DO optimizer
     - wal-669

--- src/nodeconductor_cost_planning/plugins/digitalocean.py
+++ src/nodeconductor_cost_planning/plugins/digitalocean.py
@@ -1,8 +1,10 @@
 """ Defines how to optimize DigitalOcean droplets sizes """
 import collections
 
+from django.contrib.contenttypes.models import ContentType
 from rest_framework import serializers as rf_serializers
 
+from nodeconductor.cost_tracking import models as cost_tracking_models
 from nodeconductor_digitalocean import apps as do_apps, models as do_models, serializers as do_serializers
 
 from .. import optimizers, register, serializers
@@ -20,27 +22,49 @@ OptimizedDigitalOcean = optimizers.namedtuple_with_defaults(
 class DigitalOceanOptimizer(optimizers.Optimizer):
     """ Find the cheapest Digital Ocean size for each preset """
     HOURS_IN_DAY = 24
+    DAYS_IN_MONTH = 30
+
+    def _get_service_price_list_items(self, service, resource_model):
+        """ Return all price list item that belongs to given service """
+        resource_content_type = ContentType.objects.get_for_model(resource_model)
+        default_items = set(cost_tracking_models.DefaultPriceListItem.objects.filter(
+            resource_content_type=resource_content_type))
+        items = set(cost_tracking_models.PriceListItem.objects.filter(
+            default_price_list_item__in=default_items, service=service).select_related('default_price_list_item'))
+        rewrited_defaults = set([i.default_price_list_item for i in items])
+        return items | (default_items - rewrited_defaults)
+
+    def _get_size_prices(self, service):
+        """ Return dicti with items <size>: <size price> """
+        sizes = do_models.Size.objects.all()
+        size_prices = {item.key: item.value for item in
+                       self._get_service_price_list_items(service, do_models.Droplet)}
+        return {size: size_prices.get(size.name, size.price) * self.HOURS_IN_DAY for size in sizes}
 
     def optimize(self, deployment_plan, service):
         optimized_presets = []
         price = 0
+        size_prices = self._get_size_prices(service)
         for item in deployment_plan.items.all():
             preset = item.preset
-            try:
-                size = do_models.Size.objects.filter(
-                    cores__gte=preset.cores, ram__gte=preset.ram, disk__gte=preset.storage).order_by('price')[0]
-            except IndexError:
+            sizes = do_models.Size.objects.filter(
+                cores__gte=preset.cores, ram__gte=preset.ram, disk__gte=preset.storage)
+            if not sizes:
                 preset_as_str = '%s (cores: %s, ram %s MB, storage %s MB)' % (
                     preset.name, preset.cores, preset.ram, preset.storage)
                 raise optimizers.OptimizationError(
                     'It is impossible to create a droplet for preset %s. It is too big.' % preset_as_str)
+            try:
+                size = min(sizes, key=lambda size: size_prices[size])
+            except ValueError:
+                raise optimizers.OptimizationError('Price for size %s is not defined' % size.name)
             optimized_presets.append(OptimizedPreset(
                 preset=preset,
                 size=size,
                 quantity=item.quantity,
-                price=size.price * item.quantity * self.HOURS_IN_DAY,
+                price=size_prices[size] * item.quantity,
             ))
-            price += size.price * item.quantity * self.HOURS_IN_DAY
+            price += size_prices[size] * item.quantity
         return OptimizedDigitalOcean(price=price, service=service, optimized_presets=optimized_presets)
 
 
