6b7bfca2223875a3624bea1e509c4641a8255cd6
Jenkins <jenkins@opennodecloud.com>
2017-05-23 20:48:20 +0000

    Preparing new release: 0.3.0

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ install_requires = [
 
 setup(
     name='nodeconductor-cost-planning',
-    version='0.2.0',
+    version='0.3.0',
     author='OpenNode Team',
     author_email='info@opennodecloud.com',
     url='http://nodeconductor.com',
