408f8902a7286bf22ca329fd0f45fe39b60e2047
Jenkins <jenkins@opennodecloud.com>
2017-07-03 19:50:50 +0000

    Preparing new release: 0.4.2

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ install_requires = [
 
 setup(
     name='nodeconductor-cost-planning',
-    version='0.4.1',
+    version='0.4.2',
     author='OpenNode Team',
     author_email='info@opennodecloud.com',
     url='http://waldur.com',
