7394dec0850d5004971d2a368fa04bf0997ae1f5
Jenkins <jenkins@opennodecloud.com>
2017-12-01 18:41:25 +0000

    Preparing new release: 0.5.6

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ install_requires = [
 
 setup(
     name='waldur-cost-planning',
-    version='0.5.5',
+    version='0.5.6',
     author='OpenNode Team',
     author_email='info@opennodecloud.com',
     url='http://waldur.com',
